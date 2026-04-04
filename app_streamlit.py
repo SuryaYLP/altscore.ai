3fac4d28e387a1502b88c20feaa1dee5fcb613fc
SuryaYLP <surya.ylp@gmail.com>
2026-04-04 20:11:12 +0530

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -663,7 +663,7 @@ if st.session_state.results is not None:
    
     
     from reportlab.pdfgen import canvas
-    def draw_border(canvas, doc):
+ def draw_border(canvas, doc):
     canvas.saveState()
     
     canvas.setStrokeColor(colors.HexColor("#1f4e79"))
